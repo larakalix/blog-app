1ca5aa4fb789f85cddd54767cb20bce4ff10439e
Ivan Lara <ukikalix@gmail.com>
2021-10-18 01:41:34 -0600

    Fix tag bagde clickable styles

--- components/posts/TagBadge.tsx
+++ components/posts/TagBadge.tsx
@@ -5,9 +5,9 @@ import { SparklesIcon } from "@heroicons/react/solid";
 
 const TagBadge = ({ id, title, mark }: Tag) => {
   return (
-    <li className="relative inline-flex items-center border border-gray-200 text-xs text-gray-600 font-light rounded-full px-4 py-2 m-1 hover:text-white hover:bg-pink-500 hover:border-pink-600">
+    <li className="relative inline-flex items-center">
       <Link href={`/tags/${encodeURIComponent(title)}`}>
-        <a>
+        <a className="border border-gray-200 text-xs text-gray-600 font-light rounded-full px-4 py-2 m-1 hover:text-white hover:bg-pink-500 hover:border-pink-600">
           <p className="capitalize flex items-center">
             #{title}
             {mark && <SparklesIcon width={14} className="ml-2 text-yellow-400" />}
